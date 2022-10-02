9c9e26aa89cab6174d50a90ddfbf600099d5d6db
stephaniepuertas <puertasstephanie@gmail.com>
2022-10-02 15:27:32 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, session, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.post('/process')
+def insert_info():
+        session['name']= request.form['name']
+        session['dojo_location']= request.form['dojo_location']
+        session['fav_language'] = request.form ['fav_language']
+        session['comment']= request.form['comment']
+        return redirect('/result')
+
+@app.route('/result')
+def result():
+    return render_template('/result.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=5000)
